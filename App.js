23fe65a21b0a09936c89a8c6eed0f283cbd15461
Sroy0140 <129583931+Sroy0140@users.noreply.github.com>
2024-02-07 21:09:58 +0530

    Update App.js
    added ./

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from 'Header.js';
-import HomePage from 'HomePage.js';
-import Footer from 'Footer.js';
-import FractionalKnapsackPage from 'FractionalKnapsackPage.js';
-import 'App.css';
-import JobSequencingPage from 'JobSequencingPage.js';
-import HuffmanCodingPage from 'HuffmanCodingPage.js';
-import DijkstraPage from 'DijkstraPage.js';
+import Header from './Header.js';
+import HomePage from './HomePage.js';
+import Footer from './Footer.js';
+import FractionalKnapsackPage from './FractionalKnapsackPage.js';
+import './App.css';
+import JobSequencingPage from './JobSequencingPage.js';
+import HuffmanCodingPage from './HuffmanCodingPage.js';
+import DijkstraPage from './DijkstraPage.js';
 
 function App() {
   return (
